bb89ac7489a4fcf73754505ddd6ae7770e1f364d
Brianjp93 <brianjp93@gmail.com>
2014-11-22 04:10:02 -0800

    bit position

--- bitPosition/bitPosition.py
+++ bitPosition/bitPosition.py
@@ -2,9 +2,9 @@ import sys
 with open(sys.argv[1], 'r') as f:
 	for line in f:
 		nums = line.split(",")
-		num, p1, p2 = int(nums[0]), int(nums[1]), int(nums[2])
+		num, p1, p2 = int(nums[0]), int(nums[1]) - 1, int(nums[2]) - 1
 		bit = "{0:b}".format(num)
-		if bit[p1] == bit[p2]:
+		if bit[::-1][p1] == bit[::-1][p2]:
 			print("true")
 		else:
 			print("false")
